c457c4fc8e511b4dffd98aac7591279b879f8b83
Pierre-Yves Paquette <paquette.py@gmail.com>
2016-03-09 14:27:20 -0500

    merge fix

--- source_website/fulfillment.js
+++ source_website/fulfillment.js
@@ -23,62 +23,6 @@ function extractDomain(url) {
 }
 
 function getWebDriver(source_domain){
-<<<<<<< HEAD
-  console.log(source_domain);
-  var webDriver = _.find(webDrivers, function(object) {
-    return object.host == source_domain;
-  });
-  if(!webDriver) {
-    return new BaseWebDriver();
-  }
-  else {
-    return webDriver.getWebDriver();
-  }
-}
-
-function copyToClipboard(elem) {
-  // Create hidden text element, if it doesn't already exist
-  var targetId = "_hiddenCopyText_";
-  //console.log(document.body);
-  //console.log("element: ");
-  //console.log(elem);
-  var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-  var origSelectionStart, origSelectionEnd;
-  if (isInput) {
-    // Can just use the original source element for the selection and copy
-    target = elem;
-    origSelectionStart = elem.selectionStart;
-    origSelectionEnd = elem.selectionEnd;
-  } else {
-    // Must use a temporary form element for the selection and copy
-    target = document.getElementById(targetId);
-    //console.log(target);
-    //console.log(!target);
-    if (!target) {
-      var target = document.createElement("textarea");
-      target.style.position = "absolute";
-      target.style.left = "-9999px";
-      target.style.top = "0";
-      target.id = targetId;
-      document.body.appendChild(target);
-    }
-    target.textContent = elem.textContent;
-  }
-  // Select the content
-  var currentFocus = document.activeElement;
-  target.focus();
-  target.setSelectionRange(0, target.value.length);
-
-  // Copy the selection
-  var succeed;
-  try {
-    succeed = document.execCommand("copy");
-  } catch(e) {
-    succeed = false;
-  }
-=======
->>>>>>> py
-
   console.log(source_domain);
   //var webDriver = _.find(webDrivers, function(object) {
   //  return object.host == source_domain;
@@ -94,8 +38,6 @@ function copyToClipboard(elem) {
   return web_driver;
 }
 
-
-<<<<<<< HEAD
 function removeHighlight(elem) {
   $(elem).css("background-color", "");
 }
@@ -110,50 +52,6 @@ function pasteStringInElem(elem){
   }
 }
 
-//function setIndex(index) {
-//  console.log("new index: " + index);
-//  highlight
-//}
-var web_driver;
-
-chrome.runtime.sendMessage({get_order_data: true}, function(response){
-  $.get(chrome.extension.getURL("source_website/fulfillment_overlay.html"), function(body){
-    console.log("MESSAGE RECEIVED");
-
-    // Get web driver for this domain
-    console.log(web_driver);
-    //console.log(!web_driver);
-    if(!web_driver) {
-      web_driver = getWebDriver(response.order_data.domain_host);
-      web_driver.mainInit(body, response);
-    }
-
-
-    // Change the text of the button to indicate that everything is ready to go
-    setTimeout(function() {
-      web_driver.copyCombo.text("Start copy combo!");
-    }, 2500);
-
-    /* Keyboard shortcuts */
-    //var altDown = false;
-    //
-    //$(document).keydown(altDown, function(event) {
-    //  if(event.keyCode == 18) {
-    //    event.altDown = true;
-    //  }
-    //  else if(event.altDown && event.keyCode == 81) {
-    //    console.log("ALT+Q keybind!"); // doesn't work! :(
-    //  }
-    //  //console.log(event.keyCode);
-    //});
-    //$(document).keyup(altDown, function(event) {
-    //  if(event.keyCode == 18) {
-    //    event.altDown = false;
-    //  }
-    //  //console.log(event.keyCode);
-    //});
-
-=======
 //get web driver for this domain
 $(document).ready(function(){
   var web_driver = getWebDriver(extractDomain(window.location.href));
@@ -207,6 +105,5 @@ $(document).ready(function(){
         });*/
       });
     });
->>>>>>> py
   });
 });
